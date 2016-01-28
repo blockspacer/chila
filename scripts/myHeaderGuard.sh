91142658820d8dc3246250dfc8aa3b095f126a7d
Roberto Gimenez <chilabot@gmail.com>
2016-01-28 10:38:30 -0300

    (split) subtrees/chila/scripts

---
+++ scripts/myHeaderGuard.sh
@@ -0,0 +1,2 @@
+headerGuard -e -p _CHILA -p CHILA_ -p PY_COM_PERSONAL_ -p _PY_COM_PERSONAL -s _HPP -s _H -f $1
+
---
+++ scripts/saveWork.sh
@@ -0,0 +1,6 @@
+./src/chila/tools/gitCommentCreator/gitCommentCreator.py scripts/cnf/gitCC.py
+
+echo
+git push dbox master 
+git push
+
