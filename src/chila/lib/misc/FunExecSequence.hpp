ac63bfc3ccbc38bffabc8d0512dd0341b1a967c1
Roberto Gimenez <chilabot@gmail.com>
2016-08-17 14:56:51 -0400

    (split) chila.lib.misc

--- src/chila/lib/misc/FunExecSequence.hpp
+++ src/chila/lib/misc/FunExecSequence.hpp
@@ -27,31 +27,13 @@
 MY_NSP_START
 {
     template <typename Fun0, typename Fun1>
-    struct FunExecSequence
+    inline auto funExecSequence(const Fun0 &fun0, const Fun1 &fun1)
     {
-        Fun0 fun0;
-        Fun1 fun1;
-        FunExecSequence(const Fun0 &fun0, const Fun1 &fun1) : fun0(fun0), fun1(fun1) {}
-
-#define py_com_personal_lib_misc__def_operator(z, n, data) \
-        BOOST_PP_IF(n, template<,)                                              \
-        BOOST_PP_ENUM_PARAMS(n, typename Arg)                                   \
-        BOOST_PP_IF(n, >,)                                                      \
-        void operator()(BOOST_PP_ENUM_BINARY_PARAMS(n, Arg, &arg))              \
-        {                                                                       \
-            boost::unwrap_ref(fun0)(BOOST_PP_ENUM_PARAMS(n, arg));                                 \
-            boost::unwrap_ref(fun1)(BOOST_PP_ENUM_PARAMS(n, arg));                                 \
-        }
-
-        BOOST_PP_REPEAT(50, py_com_personal_lib_misc__def_operator,);
-    };
-
-#undef py_com_personal_lib_misc__def_operator
-
-    template <typename Fun0, typename Fun1>
-    inline FunExecSequence<Fun0, Fun1> funExecSequence(const Fun0 &fun0, const Fun1 &fun1)
-    {
-        return FunExecSequence<Fun0, Fun1>(fun0, fun1);
+        return [fun0, fun1](auto&&... args)
+        {
+            boost::unwrap_ref(fun0)(std::forward<decltype(args)>(args)...);
+            boost::unwrap_ref(fun1)(std::forward<decltype(args)>(args)...);
+        };
     }
 
     template <typename FunFrom, typename FunTo>
