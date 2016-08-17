643c5d458ecc9da5c330c99bcbf91df332f85078
Roberto Gimenez <chilabot@gmail.com>
2016-08-17 14:56:59 -0400

    (split) chila.connectionTools.lib.other

--- src/chila/connectionTools/lib/other/FunExecSequence.hpp
+++ src/chila/connectionTools/lib/other/FunExecSequence.hpp
@@ -14,29 +14,12 @@
 MY_NSP_START
 {
     template <typename Fun1>
-    struct FunExecSequence
+    inline auto funExecSequence(const Fun1 &fun1)
     {
-        template <typename Fun0>
-        struct result_type
+        return [fun1](const auto &fun0)
         {
-            typedef chila::lib::misc::FunExecSequence<Fun0, Fun1> type;
+            return chila::lib::misc::funExecSequence(fun0, fun1);
         };
-
-        Fun1 fun1;
-
-        FunExecSequence(const Fun1 &fun1) : fun1(fun1) {}
-
-        template <typename Fun0>
-        typename result_type<Fun0>::type operator()(const Fun0 &fun0) const
-        {
-            return typename result_type<Fun0>::type(fun0, fun1);
-        }
-    };
-
-    template <typename Fun1>
-    inline FunExecSequence<Fun1> funExecSequence(const Fun1 &fun1)
-    {
-        return FunExecSequence<Fun1>(fun1);
     }
 
 }
